d494a372bb210f5c7b7830a60f14e2ff6c49475c
Callan Barrett <callan@zoocar.org>
2025-09-14 08:37:58 +0800

    Lint issues

--- device_windows_test.go
+++ device_windows_test.go
@@ -1,12 +1,27 @@
 // go-pn532
 // Copyright (c) 2025 The Zaparoo Project Contributors.
 // SPDX-License-Identifier: LGPL-3.0-or-later
+//
+// This file is part of go-pn532.
+//
+// go-pn532 is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// go-pn532 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with go-pn532; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 package pn532
 
 import (
 	"context"
-	"runtime"
 	"strings"
 	"testing"
 	"time"
@@ -27,16 +42,20 @@ func TestWindowsSAMConfigurationRetry(t *testing.T) {
 	// Make SAM configuration fail with no ACK error (simulating Windows issue)
 	transport.SetError(testutil.CmdSAMConfiguration, NewNoACKError("waitAck", "COM6"))
 
-	device := NewDevice(transport)
+	device, err := New(transport)
+	if err != nil {
+		t.Fatalf("Failed to create device: %v", err)
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
 	// This should fail initially since we haven't implemented retry yet
-	err := device.InitContext(ctx)
+	err = device.InitContext(ctx)
 
 	// Currently this will fail - we expect this test to fail until we implement retry
 	if err == nil {
-		t.Errorf("Expected SAM configuration to fail initially (before retry implementation)")
+		t.Error("Expected SAM configuration to fail initially (before retry implementation)")
 	}
 
 	// Verify the error message contains the expected text
