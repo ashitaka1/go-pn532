5cb1f9a73a94680efc76cad9ee22f85d09c6181a
Callan Barrett <callan@zoocar.org>
2026-01-01 08:32:35 +0800

    chore: update copyright headers to 2026

--- tagops/writer.go
+++ tagops/writer.go
@@ -1,4 +1,4 @@
-// Copyright 2025 The Zaparoo Project Contributors.
+// Copyright 2026 The Zaparoo Project Contributors.
 // SPDX-License-Identifier: Apache-2.0
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
